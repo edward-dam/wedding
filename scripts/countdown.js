07e6224a1e8d4972a68c10be520feddce98a20a1
Edward Dam <edward.dam@gmail.com>
2022-04-15 22:00:42 +0100

    Create countdown.js

---
+++ scripts/countdown.js
@@ -0,0 +1,17 @@
+// Author: Ed Dam
+
+var countDownDate = new Date("May 11, 2022 00:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("Timer").innerHTML = days + " days " + hours + " hrs "
+  + minutes + " mins " + seconds + " secs ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("Timer").innerHTML = "Countdown Expired!";
+  }
+}, 1000);
