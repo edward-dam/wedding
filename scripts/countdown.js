7f74d50c995f0a8518ca2b36cb7238a2ced26c51
Edward Dam <edward.dam@gmail.com>
2022-05-09 14:33:52 +0100

    Updated countdown time to 2pm

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,6 +1,6 @@
 // Author: Ed Dam
 
-var countDownDate = new Date("May 11, 2022 00:00:00").getTime();
+var countDownDate = new Date("May 11, 2022 14:00:00").getTime();
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
