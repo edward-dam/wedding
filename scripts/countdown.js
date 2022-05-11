dc9b857fe56d3c74872df232bf9185793197eb21
Edward Dam <edward.dam@gmail.com>
2022-05-11 13:53:51 +0100

    Update timer to count upwards

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,17 +1,12 @@
 // Author: Ed Dam
 
-var countDownDate = new Date("May 11, 2022 14:00:00").getTime();
+var weddingDate = new Date("May 11, 2022 14:00:00").getTime();
 var x = setInterval(function() {
   var now = new Date().getTime();
-  var distance = countDownDate - now;
+  var distance = now - weddingDate;
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("Timer").innerHTML = days + " days " + hours + " hrs "
-  + minutes + " mins " + seconds + " secs ";
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("Timer").innerHTML = "0 days 0 hrs 0 mins 0 secs";
-  }
+  document.getElementById("Timer").innerHTML = days + " days " + hours + " hours "  + minutes + " mins " + seconds + " secs ";
 }, 1000);
