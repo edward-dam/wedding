7687ebbd5475ee4bf8ae0ae6b1fbb9db280b578c
Edward Dam <edward.dam@gmail.com>
2022-05-14 11:41:14 +0100

    Updated with home slides

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,7 +1,30 @@
 // Author: Ed Dam
 
+// Slides
+
+var slideHomeIndex = 1;
 var slidePhotosIndex = 1;
+showHomeDivs(slideHomeIndex);
 showPhotosDivs(slidePhotosIndex);
+
+// Home
+
+function slideHomePlus(n) {
+  showHomeDivs(slideHomeIndex += n);
+}
+
+function showHomeDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("wed-slide-home");
+  if (n > x.length) {slideHomeIndex = 1}
+  if (n < 1) {slideHomeIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideHomeIndex-1].style.display = "block";
+}
+
+// Photos
 
 function slidePhotosPlus(n) {
   showPhotosDivs(slidePhotosIndex += n);
